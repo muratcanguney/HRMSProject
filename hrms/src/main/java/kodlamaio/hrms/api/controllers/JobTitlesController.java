7e9a00df441a55b7c0a598cf4af135e39f148f0f
Murat <muratcan.guney@hotmail.com>
2021-05-24 01:58:18 +0300

    HRMS Projesine Başlandı..

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/JobTitlesController.java
@@ -0,0 +1,26 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.JobTitleService;
+import kodlamaio.hrms.entities.concretes.JobTitle;
+
+@RestController
+@RequestMapping("api/jobtitles")
+public class JobTitlesController {
+
+	private JobTitleService jobTitleService;
+
+	public JobTitlesController(JobTitleService jobTitleService) {
+		this.jobTitleService = jobTitleService;
+	}
+
+	@GetMapping("/getall")
+	public List<JobTitle> getAll() {
+		return this.jobTitleService.getAll();
+	}
+}
